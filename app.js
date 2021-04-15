bbfd9f393575552257c775d5803105ca982be0cf
Sujeet Thakur <mydeveloper.sujeet@gmail.com>
2021-04-15 23:25:15 +0530

    Created First api call and parsed json data

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
 const { json } = require('express')
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-app.get('/', (req, res) =>{
-    res.send("API first get request")
+//middleware
+app.use(bodyParser.json())
+
+require('dotenv/config')
+
+const api = process.env.API_URL
+
+app.get(`${api}/product`, (req, res) =>{
+
+    const product = {
+        id : 1,
+        name: "Sujeet Thakur",
+        image: 'Some_url',
+    }
+
+    res.send(product)
+})
+
+app.post(`${api}/product`, (req, res) =>{
+
+   const newProduct = req.body
+
+    res.send(newProduct)
+
 })
 
 app.listen(3000, ()=>{
